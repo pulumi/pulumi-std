a441fca040b61937137bffd23fc748e726bd36b1
aq17 <aqiu@pulumi.com>
2022-11-09 14:59:20 -0800

    add reverse, sort, trim, trimprefix, trimspace, trimsuffix, urlencode

--- tests/main.go
+++ tests/main.go
@@ -119,6 +119,8 @@ func expectedOutputs() map[string]interface{} {
 		"slice":                []int{1, 2, 3},
 		"parseint":             100,
 		"parseintWithBase":     255,
+		"reverse":              []interface{}{"foo", 1, true},
+		"sort":                 []string{"Avocado", "Orange", "apple", "banana", "orange", "pear", "watermelon"},
 		"startswith":           true,
 		"endswith":             true,
 		"values":               []int{1, 2},
@@ -126,7 +128,12 @@ func expectedOutputs() map[string]interface{} {
 		"strrev":               "olleh",
 		"substr":               "hello",
 		"title":                "Hello World",
+		"trim":                 "Hello  World!",
+		"trimprefix":           " bar",
+		"trimsuffix":           "foo ",
+		"trimspace":            "foobar",
 		"upper":                "ONE",
+		"urlencode":            "Hello+World%21",
 	}
 }
 
