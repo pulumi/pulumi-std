5e1b6dccbb721f54f8708f40fc3c9c075942230e
Will Jones <will@sacharissa.co.uk>
2025-02-10 17:22:59 +0000

    Improve `format`'s compatibility with Terraform
    We want to start using `format` in `pulumi convert`. This change
    improves its compatibility with the Terraform function of the same name,
    bulking out the test suite in the process to prove it.
    
    Part of pulumi/pulumi#18521

--- std/format.go
+++ std/format.go
@@ -1,4 +1,4 @@
-// Copyright 2022, Pulumi Corporation.
+// Copyright 2025, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,17 +15,21 @@
 package std
 
 import (
+	"encoding/json"
 	"fmt"
+	"math"
 
 	p "github.com/pulumi/pulumi-go-provider"
 	"github.com/pulumi/pulumi-go-provider/infer"
 )
 
-type Format struct{}
-type FormatArgs struct {
-	Input string        `pulumi:"input"`
-	Args  []interface{} `pulumi:"args"`
-}
+type (
+	Format     struct{}
+	FormatArgs struct {
+		Input string        `pulumi:"input"`
+		Args  []interface{} `pulumi:"args"`
+	}
+)
 
 type FormatResult struct {
 	Result string `pulumi:"result"`
@@ -35,6 +39,57 @@ func (r *Format) Annotate(a infer.Annotator) {
 	a.Describe(r, "Formats a string according to the given format. The syntax for the format is standard sprintf syntax.")
 }
 
+// Call implements the `format` function. It is designed to match the behaviour of the Terraform `format` function. The
+// implementation of Terraform's function is taken from `go-cty`, and lives at
+// https://github.com/zclconf/go-cty/blob/main/cty/function/stdlib/format.go. When making changes here to ensure
+// compatibility, this source can be used as a reference (and is, for instance, the source of existing checks such as
+// whether numbers are floating-point or integers).
 func (*Format) Call(_ p.Context, args FormatArgs) (FormatResult, error) {
-	return FormatResult{fmt.Sprintf(args.Input, args.Args...)}, nil
+	processedArgs := make([]interface{}, len(args.Args))
+	for i, arg := range args.Args {
+		processedArgs[i] = formattable{value: arg}
+	}
+
+	return FormatResult{fmt.Sprintf(args.Input, processedArgs...)}, nil
+}
+
+// formattable is a type that wraps a value and implements the fmt.Formatter interface. This allows us to control how
+// values are formatted when passed to fmt.Sprintf, so that we can match the behaviour of Terraform's `format` function.
+type formattable struct {
+	value interface{}
+}
+
+// Format implements the fmt.Formatter interface's Format function.
+func (f formattable) Format(s fmt.State, verb rune) {
+	// All numbers come in over the wire as float64, but for integer-based format specifiers such as %b, %o, %x, etc. to
+	// work correctly, we'll need to spot these and convert them to integer representations before delegating to
+	// fmt.Sprintf. This means that things like %d, will work on values like 1.0, which can be converted into integers.
+	// This is the same behaviour as Terraform's format function, though it does differ from e.g. Go's fmt.Sprintf, which
+	// will refuse to format a float as an integer.
+	v := f.value
+	if fv, ok := v.(float64); ok {
+		if fv == math.Trunc(fv) && fv <= math.MaxInt64 && fv >= math.MinInt64 {
+			v = int64(fv)
+		}
+	}
+
+	// If we've been asked to print a map or slice, we always convert it to JSON. Technically, we need to convert all
+	// values to JSON when the format specifier is #v, but it turns out that all types apart from maps and slices already
+	// work correctly in this case.
+	if verb == 'v' {
+		switch v.(type) {
+		case map[string]interface{}, []interface{}:
+			bytes, err := json.Marshal(v)
+			if err != nil {
+				fmt.Fprintf(s, "<error: %v>", err)
+				return
+			}
+
+			fmt.Fprint(s, string(bytes))
+			return
+		}
+	}
+
+	// In all other cases, we can just delegate to fmt.Sprintf as-is, which should work correctly.
+	fmt.Fprintf(s, fmt.FormatString(s, verb), v)
 }
